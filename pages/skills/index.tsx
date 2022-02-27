772ad74acf9d5bf8578e9f162f8647c1ee01fb80
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2022-02-27 23:16:15 +0530

    lint issue again

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -7,12 +7,12 @@ import { showIcon } from '../../lib/skills/show_icon';
 import styles from './skills.module.scss';
 
 export async function getStaticProps() {
-    const skillsData = await getSkillsData();
-    return {
-        props: {
-            skillsData: skillsData[0],
-        },
-    };
+  const skillsData = await getSkillsData();
+  return {
+    props: {
+      skillsData: skillsData[0],
+    },
+  };
 }
 
 interface contentProps {
@@ -36,41 +36,41 @@ interface Props {
 
 const Skills: React.FC<Props> = (props) => {
 
-    const { skillsData } = props;
-    const { heading, about, content } = skillsData || {};
+  const { skillsData } = props;
+  const { heading, about, content } = skillsData || {};
 
-    return (
-        <Layout page="">
-            <Box className={styles.skills_main}>
-                <Box className="d-flex flex-column align-items-center flex-wrap">
-                    <style jsx>{`
+  return (
+    <Layout page="">
+      <Box className={styles.skills_main}>
+        <Box className="d-flex flex-column align-items-center flex-wrap">
+          <style jsx>{`
                     h3 {
                         margin-top: 50px;
                     }
                 `}</style>
-                    <h3>{heading}</h3>
-                    <p className="w-50">
-                        {about}
-                    </p>
-                </Box>
-                <Box className='d-flex flex-wrap justify-content-evenly'>
-                    {content.map(item => {
-                        const { label, heading, listData } = item || {};
-                        return <Box className="d-flex flex-column flex-wrap ms-5 w-25 mt-3">
-                            {label && showIcon(label)}
-                            {label && <h5 className="mt-3">{label}</h5>}
-                            {heading && <p>
-                                {heading}
-                            </p>}
-                            {listData && <ul className="list-unstyled list-line">
-                                {ReactHtmlParser(listData)}
-                            </ul>}
-                        </Box>
-                    })}
-                </Box>
-            </Box>
-        </Layout>
-    );
-}
+          <h3>{heading}</h3>
+          <p className="w-50">
+            {about}
+          </p>
+        </Box>
+        <Box className="d-flex flex-wrap justify-content-evenly">
+          {content.map(item => {
+            const { label, heading, listData } = item || {};
+            return <Box className="d-flex flex-column flex-wrap ms-5 w-25 mt-3">
+              {label && showIcon(label)}
+              {label && <h5 className="mt-3">{label}</h5>}
+              {heading && <p>
+                {heading}
+              </p>}
+              {listData && <ul className="list-unstyled list-line">
+                {ReactHtmlParser(listData)}
+              </ul>}
+            </Box>;
+          })}
+        </Box>
+      </Box>
+    </Layout>
+  );
+};
 
 export default Skills;
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 import Layout from '../../components/layout';
 import styles from './work.module.scss';
 // import uhro_logo from '../../public/images/unicorn_logo.png';
@@ -10,12 +10,12 @@ import { showImage } from '../../lib/work/show_image';
 import ReactHtmlParser from 'react-html-parser';
 
 export const getStaticProps = async () => {
-    const workData = await getWorkData();
-    return {
-        props: {
-            workData: workData[0],
-        }
-    }
+  const workData = await getWorkData();
+  return {
+    props: {
+      workData: workData[0],
+    },
+  };
 };
 
 interface contentProps {
@@ -35,59 +35,59 @@ interface workProps {
 }
 
 const work: React.FC<workProps> = (props) => {
-    const [currentPage, setCurrentPage] = React.useState(0);
-    const [content] = React.useState(props?.workData?.content);
-    const [totalPage] = React.useState(content?.length);
-    const changeProject = (e: number): void => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-        setCurrentPage(Number(e) - 1);
-    }
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const [content] = React.useState(props?.workData?.content);
+  const [totalPage] = React.useState(content?.length);
+  const changeProject = (e: number): void => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    setCurrentPage(Number(e) - 1);
+  };
 
-    return (
-        <Layout page="">
-            <div className={styles.work_main}>
-                {content && <div className={'d-flex flex-wrap ms-5 mt-3 ' + styles.work_main__office}>
-                    <div className={styles.work_main__UHRO}>
-                        <h2>{content[currentPage]?.project_name}</h2>
-                        <p className='w-50 mt-2'>
-                            {content[currentPage]?.project_desc}
-                        </p>
-                        <div className='d-flex flex-wrap mt-4'>
-                            <div className={"d-flex justify-content-center align-items-center " + styles.work_main__image} >
-                                <Image src={showImage(content[currentPage]?.image)} alt="uhro_logo" />
-                            </div>
-                            <div className='w-25 ms-4'>
-                                <p className={styles.work_main__product}>
-                                    {content[currentPage]?.product}
-                                </p>
-                                <p className={styles.work_main__role}>
-                                    {ReactHtmlParser(content[currentPage]?.role)}
-                                </p>
-                                <p className='mt-3'>
-                                    {content[currentPage]?.work_done}
-                                </p>
-                                <h3 className={"mt-4 mb-3 " + styles.work_main__wid}>What I did</h3>
-                                <ul className="list-unstyled list-line mb-4">
-                                    {ReactHtmlParser(content[currentPage]?.responsibility)}
-                                </ul>
-                                <br></br>
-                                {content[currentPage]?.link !== 'N/A' && <Link href={content[currentPage]?.link} passHref={true}>
-                                    <a target="_blank" className={"mt-5 " + styles.work_main__link}>
+  return (
+    <Layout page="">
+      <div className={styles.work_main}>
+        {content && <div className={'d-flex flex-wrap ms-5 mt-3 ' + styles.work_main__office}>
+          <div className={styles.work_main__UHRO}>
+            <h2>{content[currentPage]?.project_name}</h2>
+            <p className="w-50 mt-2">
+              {content[currentPage]?.project_desc}
+            </p>
+            <div className="d-flex flex-wrap mt-4">
+              <div className={'d-flex justify-content-center align-items-center ' + styles.work_main__image}>
+                <Image src={showImage(content[currentPage]?.image)} alt="uhro_logo" />
+              </div>
+              <div className="w-25 ms-4">
+                <p className={styles.work_main__product}>
+                  {content[currentPage]?.product}
+                </p>
+                <p className={styles.work_main__role}>
+                  {ReactHtmlParser(content[currentPage]?.role)}
+                </p>
+                <p className="mt-3">
+                  {content[currentPage]?.work_done}
+                </p>
+                <h3 className={'mt-4 mb-3 ' + styles.work_main__wid}>What I did</h3>
+                <ul className="list-unstyled list-line mb-4">
+                  {ReactHtmlParser(content[currentPage]?.responsibility)}
+                </ul>
+                <br></br>
+                {content[currentPage]?.link !== 'N/A' && <Link href={content[currentPage]?.link} passHref={true}>
+                  <a target="_blank" className={'mt-5 ' + styles.work_main__link}>
                                         VISIT WEBSITE
-                                    </a>
-                                </Link>}
-                            </div>
-                        </div>
-                    </div>
-                </div>}
+                  </a>
+                </Link>}
+              </div>
             </div>
-            <div className={'d-flex mt-4 mb-3 justify-content-center ' + styles.work_pagination}>
-                <Pagination size="small" total={4} responsive={true} simple={true}
-                    defaultPageSize={1} onChange={changeProject} />
-            </div>
-        </Layout>
-    );
-}
+          </div>
+        </div>}
+      </div>
+      <div className={'d-flex mt-4 mb-3 justify-content-center ' + styles.work_pagination}>
+        <Pagination size="small" total={4} responsive={true} simple={true}
+          defaultPageSize={1} onChange={changeProject} />
+      </div>
+    </Layout>
+  );
+};
 
 export default work;
 
